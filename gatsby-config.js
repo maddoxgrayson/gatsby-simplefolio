cc4dce35df5cec7049ac28e95194a7297811f11b
maddoxgrayson <samira.gibsonchc@gmail.com>
2020-11-11 22:38:43 -0500

    changed site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Samira Gibson`,
+        short_name: `Samira Gibson`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
