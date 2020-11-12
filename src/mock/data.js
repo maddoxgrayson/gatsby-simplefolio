473fe727d3c996e2b9067121c23e6c15694e056c
maddoxgrayson <samira.gibsonchc@gmail.com>
2020-11-12 09:34:59 -0500

    updated contact cta

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne:
     'With a background in visual & digital design, ecommerce, action sports, music media, and video production, I have a special passion for helping black entrepreneurs craft their own visual brands. A lover of dad jokes and manga, I live at the intersection of design + code with a degree in computer technology focusing on digital media.',
   paragraphTwo:
-    "Working and living in South Philly as a design consultant, when I''m not at my desk you can find me skateboarding at Grays Ferry Skatepark, scarfing down desserts from A la Mousse, or out on walks with my lovely partner, our 7 yr old beagle Theo and 4 yr old Rottie, Rocky.",
+    "Working and living in South Philly as a design consultant, when I'm not at my desk you can find me skateboarding at Grays Ferry Skatepark, scarfing down desserts from A la Mousse, or out on walks with my lovely partner, our 7 yr old beagle Theo and 4 yr old Rottie, Rocky.",
   resume: '',
 };
 
@@ -58,7 +58,7 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
+  cta: "Work on a project? I'd love to connect!",
   btn: '',
   email: 'samira.gibsonchc@gmail.com',
 };
