23b1871d42f8c78bb5e14bfe1e4d2494f2a1b614
maddoxgrayson <samira.gibsonchc@gmail.com>
2020-11-13 16:09:09 -0500

    fix, remove project data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import Hero from './Hero/Hero';
 import About from './About/About';
-import Projects from './Projects/Projects';
+import Projects from './Projects/Projects'; 
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
 
 import { PortfolioProvider } from '../context/context';
 
-import { heroData, aboutData, projectsData, contactData, footerData } from '../mock/data';
+import { heroData, aboutData, projectsData,  contactData, footerData } from '../mock/data';
 
 function App() {
   const [hero, setHero] = useState({});
   const [about, setAbout] = useState({});
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([]); 
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
 
   useEffect(() => {
     setHero({ ...heroData });
     setAbout({ ...aboutData });
-    setProjects([...projectsData]);
+  setProjects([...projectsData]); 
     setContact({ ...contactData });
     setFooter({ ...footerData });
   }, []);
@@ -29,7 +29,7 @@ function App() {
       <Hero />
       <About />
       <Contact />
-      <Projects />
+      
       <Footer />
     </PortfolioProvider>
   );
